d7f875878547994c9052d2e700caa38d46fc0086
Volodya Mikhnov <vvvmikhnov@gmail.com>
2024-07-02 08:44:51 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
 
-export default defineConfig({
+// export default defineConfig({
 
-  base: '/Car_Dealership/',
-  server: {
-    fsServe: {
-      strict: false,
-    },
-  },
-  build: {
-    // outDir: 'public',
-    manifest: false,
-    rollupOptions: {
-      input: './public/index.html', 
-    },
-  },
-});
+//   base: '/Car_Dealership/',
+//   server: {
+//     fsServe: {
+//       strict: false,
+//     },
+//   },
+//   build: {
+//     // outDir: 'public',
+//     manifest: false,
+//     rollupOptions: {
+//       input: './public/index.html', 
+//     },
+//   },
+// });
